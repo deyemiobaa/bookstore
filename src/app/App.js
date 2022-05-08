1513af73c14dacc491a10e15c44b703b89b03f03
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-05-09 00:57:17 +0100

    use redux createStore and Combine reducers

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Categories from '../redux/categories/categories';
 import Books from '../redux/books/books';
+import NavBar from '../components/header/navigation';
 import './App.scss';
 
 class AppBody extends React.PureComponent {
   render() {
     return (
       <div className="container">
+        <NavBar />
         <Routes>
           <Route path="/" element={<Books />} />
           <Route path="/categories" element={<Categories />} />
